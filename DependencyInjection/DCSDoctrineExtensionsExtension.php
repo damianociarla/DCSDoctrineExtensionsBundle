21b3225c4a4cae09a262c9aa0b8a663f57efa0b1
Damiano Ciarla <damiano.ciarla@gmail.com>
2014-01-13 17:28:31 +0100

    Fix class name

--- DependencyInjection/DCSDoctrineExtensionsExtension.php
+++ DependencyInjection/DCSDoctrineExtensionsExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class DoctrineDoctrineExtensionExtension extends Extension
+class DCSDoctrineExtensionsExtension extends Extension
 {
     /**
      * {@inheritDoc}
